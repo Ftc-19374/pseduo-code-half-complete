10e1eca07457db444fd92fb68702dfffaac97076
Jaws-all <77344665+Jaws-all@users.noreply.github.com>
2021-11-20 08:18:20 -0500

    Update blankshinyfile.java

--- blankshinyfile.java
+++ blankshinyfile.java
@@ -1,14 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 // import statements
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 /*The purpose of this program is to move the cascading lift up or down 100 ticks based 
 on user input from controller 1, with the a and b buttons being pressed, although since this is just comments it does nothing.
 */
 // This is a TeleOp program because it needs user input.
 @TeleOp(name="Encoder movement")
 // This class name should be the same as the name of the file. That might be why there is an error compiling.
-public class blankshinyfile {
+public class blankshinyfile extends LinearOpMode{
     /*
     set variables of tick number of a gobilda 5202/5203 motor (it's in here because we need to know how far we can move with a rotation.)
     create cascading lift motor (it's the only one)
@@ -19,12 +21,13 @@ public class blankshinyfile {
     @Override
     public void runOpMode() throws InterruptedException {
         //Declare motor from hardware map
-        motor = hardwareMap.get("motor");
+        motor = hardwareMap.dcMotor.get("motor");
         //set to run using encoders
-        motor.setMode("STOP_AND_RESET_ENCODERS");
-        motor.setMode("RUN_USING_ENCODERS");
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         while(opModeIsActive()){
+            //set gamepad button values
             a = gamepad2.a;
             b = gamepad2.b;
             if(a){
